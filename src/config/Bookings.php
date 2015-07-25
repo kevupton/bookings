d8424b2cbd11768ed68322d80c9ab469b264054c
kevupton <k_upton@live.com>
2015-07-25 17:07:11 +1000

    updated the composer

--- src/config/Bookings.php
+++ src/config/Bookings.php
@@ -1,6 +1,25 @@
 <?php
 
 return array(
-    'name' => 'test',
-    'joe' => 'true'
+
+    'events' => array(
+        'table_name' => 'events',
+        'primary_key' => 'id'
+    ),
+
+    'sessions' => array(
+        'table_name' => 'events',
+        'primary_key' => 'id'
+    ),
+
+    'venues' => array(
+        'table_name' => 'events',
+        'primary_key' => 'id'
+    ),
+
+    'packages' => array(
+        'table_name' => 'packages',
+        'primary_key' => 'id'
+    ),
+
 );
