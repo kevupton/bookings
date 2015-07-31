1638448983289870243c7757438043bc0710c076
kevupton <k_upton@live.com>
2015-08-01 07:00:10 +1000

    updated provider for the seeding

--- src/Kevupton/Bookings/Providers/BookingsServiceProvider.php
+++ src/Kevupton/Bookings/Providers/BookingsServiceProvider.php
@@ -15,6 +15,9 @@ class BookingsServiceProvider extends ServiceProvider {
         $this->publishes([
             __DIR__.'/../../../database/migrations/' => database_path('migrations')
         ], 'migrations');
+        $this->publishes([
+            __DIR__.'/../../../database/seeds/' => database_path('seeds')
+        ], 'seeds');
     }
 
     /**
