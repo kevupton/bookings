1c9cb688eeb9590c5540777ad77386f2aa041adc
Kevin Upton <k_upton@live.com>
2016-04-24 04:08:50 +1000

    asadd

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,10 +5,11 @@ if [ ! -f "laravel/composer.json" ]; then
     cd laravel
     composer update
     composer require kevupton/bookings
+    awk '/'\''providers'\'' \=\> \[/ { print; print "'$PACKAGE_PROVIDER',"; next }1' \
+       config/app.php
     echo "$(awk '/'\''providers'\'' \=\> \[/ { print; print "'$PACKAGE_PROVIDER',"; next }1' \
        config/app.php)" > config/app.php
     php artisan vendor:publish
     php artisan migrate
-    echo "$PACKAGE_PROVIDER"
     cd ..
 fi
