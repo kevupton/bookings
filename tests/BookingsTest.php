731a2f5639086e9999a14f1a33ff9290da29d295
Kevin Upton <k_upton@live.com>
2016-04-24 00:21:31 +1000

    created tests folder

---
+++ tests/BookingsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class BookingsTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        echo "Testing kevupton/bookings";
+    }
+}
