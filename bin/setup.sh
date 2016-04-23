caa4927caf1b6dad197b348dd8d104a14c609a92
Kevin Upton <k_upton@live.com>
2016-04-24 03:36:49 +1000

    cache the laravel folder

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,7 +4,7 @@ if [ ! -d "laravel" ]; then
     cd laravel
     composer update
     composer require kevupton/bookings
-    echo "$(awk '/'\''providers'\'' \=\> \[/ { print; print "$PACKAGE_PROVIDER,"; next }1' \
+    echo "$(awk '/'\''providers'\'' \=\> \[/ { print; print "'$PACKAGE_PROVIDER',"; next }1' \
        config/app.php)" > config/app.php
     php artisan vendor:publish
     php artisan migrate
