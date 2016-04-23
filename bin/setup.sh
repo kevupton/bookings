1270a8d840a4aeadecbdf0ebe4929ce35f67ebcd
Kevin Upton <k_upton@live.com>
2016-04-24 03:24:13 +1000

    removed semicolons

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 if [ ! -d "laravel" ]; then
-    composer create-project laravel/laravel;
-    composer update;
-    cd laravel;
-    composer require kevupton/bookings;
-    echo "$(awk '/'\''providers'\'' \=\> \[/ { print; print "$PACKAGE_PROVIDER,"; next }1'
-       config/app.php)" > config/app.php;
-    php artisan vendor:publish;
-    php artisan migrate;
+    composer create-project laravel/laravel
+    composer update
+    cd laravel
+    composer require kevupton/bookings
+    echo "$(awk '/'\''providers'\'' \=\> \[/ { print; print "$PACKAGE_PROVIDER,"; next }1' \
+       config/app.php)" > config/app.php
+    php artisan vendor:publish
+    php artisan migrate
+    cd ..
 fi
